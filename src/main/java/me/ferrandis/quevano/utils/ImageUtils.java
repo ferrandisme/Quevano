658958c969c46d51b694dcc381e62c8b20185aaa
adferhom <adferhom@inf.upv.es>
2023-01-29 19:28:30 +0100

    [ADD] Basic test of image service

--- src/main/java/me/ferrandis/quevano/utils/ImageUtils.java
+++ src/main/java/me/ferrandis/quevano/utils/ImageUtils.java
@@ -1,12 +1,21 @@
 package me.ferrandis.quevano.utils;
 
 import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
 
 public class ImageUtils {
 
-    public static void readImage(File image){
-        //TODO
+    /** Local directory where you have tessdata_best data (github clone)*/
+    private static final String LOCAL_TESS_DIRECTORY =  "F:\\tess";
+
+    public static String readImage(File image) throws TesseractException {
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath(LOCAL_TESS_DIRECTORY);
+        tesseract.setLanguage("eng");
+        tesseract.setPageSegMode(1);
+        tesseract.setOcrEngineMode(1);
+        return tesseract.doOCR(image);
     }
 }
--- src/test/java/me/ferrandis/quevano/utils/ImageUtilsTest.java
+++ src/test/java/me/ferrandis/quevano/utils/ImageUtilsTest.java
@@ -2,6 +2,7 @@ package me.ferrandis.quevano.utils;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -10,16 +11,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ImageUtilsTest {
 
+    /** Local directory where you have tessdata_best data (github clone)*/
+    private static final String LOCAL_TESS_DIRECTORY =  "F:\\tess";
+
     @Test
     void testIsLibraryConfigured() throws TesseractException {
         File image = new File("src/test/resources/example.png");
         Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("F:\\tess");
+        tesseract.setDatapath(LOCAL_TESS_DIRECTORY);
         tesseract.setLanguage("eng");
         tesseract.setPageSegMode(1);
         tesseract.setOcrEngineMode(1);
         String result = tesseract.doOCR(image);
 
         assertTrue(result.contains("Test work OK"));
+    }
+
+
+    @Test
+    void testReadImage() throws TesseractException {
+        File image = new File("src/test/resources/example.png");
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath(LOCAL_TESS_DIRECTORY);
+        tesseract.setLanguage("eng");
+        tesseract.setPageSegMode(1);
+        tesseract.setOcrEngineMode(1);
+        String expected = tesseract.doOCR(image);
+        String result = ImageUtils.readImage(image);
+        assertEquals(expected , result, expected + " != " + result );
+    }
+
+
+    @Test
+    @Disabled
+    void testReadImagePerfectMath() throws TesseractException {
+        File image = new File("src/test/resources/example.png");
+        String result = ImageUtils.readImage(image);
+        assertTrue(result.contains("Hello") , result + " not contains Hello");
     }
 }
