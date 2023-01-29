0675b5a0007caa76e0c19aace2aecbb4e4634072
adferhom <adferhom@inf.upv.es>
2023-01-29 17:58:58 +0100

    [ADD] Project created
    https://start.spring.io/

---
+++ src/main/java/me/ferrandis/quevano/QuevanoApplication.java
@@ -0,0 +1,13 @@
+package me.ferrandis.quevano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuevanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuevanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ferrandis/quevano/QuevanoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ferrandis.quevano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuevanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
